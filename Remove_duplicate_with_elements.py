2ae9a038d734595474538dd0eb603cdc453c361f
Sakthivasan369 <145281270+Sakthivasan369@users.noreply.github.com>
2024-01-16 06:51:26 +0530

    Create Remove_duplicate_with_elements.py

---
+++ Remove_duplicate_with_elements.py
@@ -0,0 +1,4 @@
+test_tup=tuple(input("Enter The elements of the tuple :").split())
+print("The Original tuple is :",(test_tup))
+res=tuple(set(test_tup))
+print(f"The tuple after removing duplicates :{(res)}"
