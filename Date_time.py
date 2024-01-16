a83460ad817885b4585f11c9153f2c5bade5f88d
Sakthivasan369 <145281270+Sakthivasan369@users.noreply.github.com>
2024-01-16 07:26:45 +0530

    Create Date_time.py

---
+++ Date_time.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+current_time=datetime.now()
+formatted_time=current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+print(formatted_time)
+---------------------------------------------------------------------------------
+Output
+2024-01-16 07:26:26.121239
