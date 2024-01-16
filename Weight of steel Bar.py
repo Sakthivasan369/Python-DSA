a159c2b123d2f1ea06132860e95d17fda2cf928d
Sakthivasan369 <145281270+Sakthivasan369@users.noreply.github.com>
2024-01-16 06:48:08 +0530

    Weight of steel Bar.py

---
+++ Weight of steel Bar.py
@@ -0,0 +1,4 @@
+diameter=float(input("Enter the Diameter of the Rounded steel bar :"))
+length=float(input("Enter the  Length of the Rounded steel bar "))
+weight=((diameter*diameter)/162.28)*length
+print(f"Weight of Steel Bar:{weight}")
