b9df179ebe4c1dcedb131543ad513f47589fe76a
Sakthivasan369 <145281270+Sakthivasan369@users.noreply.github.com>
2024-01-16 07:03:29 +0530

    Update Calendar_month.py

--- Calendar_month.py
+++ Calendar_month.py
@@ -1,5 +1,10 @@
 import calendar
-date=input()
+date=input("Enter the date by separating it with (-):")
 d,m,y=date.split("-")
-print(calendar.month_name[int(m)])
-print(calendar.month_abbr[int(m)]
+print("The month full name:",calendar.month_name[int(m)])
+print("The month abbreviation:",calendar.month_abbr[int(m)])
+-------------------------------------------------------------------------------
+Output
+Enter the date by separating it with (-):15-08-1947
+The month full name: August
+The month abbreviation: Aug
