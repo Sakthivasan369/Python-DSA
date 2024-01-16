b7000ba1c08c89a6113e4a4d9ab8b197eea3770a
Sakthivasan369 <145281270+Sakthivasan369@users.noreply.github.com>
2024-01-16 06:57:44 +0530

    Update Remove_duplicate_with_elements.py

--- Remove_duplicate_with_elements.py
+++ Remove_duplicate_with_elements.py
@@ -1,4 +1,10 @@
 test_tup=tuple(input("Enter The elements of the tuple :").split())
 print("The Original tuple is :",(test_tup))
 res=tuple(set(test_tup))
-print(f"The tuple after removing duplicates :{(res)}"
+print(f"The tuple after removing duplicates :{(res)}")
+----------------------------------------------------------
+Output
+Enter The elements of the tuple :201 202 a 202 b ab 201
+The Original tuple is : ('201', '202', 'a', '202', 'b', 'ab', '201')
+The tuple after removing duplicates :('202', 'a', 'b', '201', 'ab')
+
